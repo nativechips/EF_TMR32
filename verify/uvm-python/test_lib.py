4450c017db30ed8e3731e6572799bd21daffb85c
M0stafaRady <mostafa.rady@efabless.com>
2024-04-15 19:51:45 +0200

    use new base test format

--- verify/uvm-python/test_lib.py
+++ verify/uvm-python/test_lib.py
@@ -9,7 +9,12 @@ from uvm.base.uvm_object_globals import UVM_FULL, UVM_LOW, UVM_ERROR
 from uvm.base.uvm_globals import run_test
 from EF_UVM.top_env import top_env
 from tmr32_interface.tmr32_if import tmr32_if
-from EF_UVM.bus_env.bus_interface.bus_if import bus_ahb_if, bus_apb_if, bus_wb_if, bus_irq_if
+from EF_UVM.bus_env.bus_interface.bus_if import (
+    bus_ahb_if,
+    bus_apb_if,
+    bus_wb_if,
+    bus_irq_if,
+)
 from cocotb_coverage.coverage import coverage_db
 from cocotb.triggers import Event, First
 from EF_UVM.bus_env.bus_regs import bus_regs
@@ -38,13 +43,16 @@ from tmr32_logger.tmr32_logger import tmr32_logger
 from EF_UVM.scoreboard import scoreboard
 from tmr32_coverage.tmr32_bus_coverage import tmr32_bus_coverage
 from EF_UVM.bus_env.bus_coverage.bus_coverage import bus_coverage
-# 
+
+#
 from EF_UVM.bus_env.bus_agent.bus_ahb_driver import bus_ahb_driver
 from EF_UVM.bus_env.bus_agent.bus_apb_driver import bus_apb_driver
 from EF_UVM.bus_env.bus_agent.bus_wb_driver import bus_wb_driver
 from EF_UVM.bus_env.bus_agent.bus_ahb_monitor import bus_ahb_monitor
 from EF_UVM.bus_env.bus_agent.bus_apb_monitor import bus_apb_monitor
 from EF_UVM.bus_env.bus_agent.bus_wb_monitor import bus_wb_monitor
+
+from EF_UVM.base_test import base_test
 
 
 @cocotb.test()
@@ -53,7 +61,7 @@ async def module_top(dut):
     # profiler.enable()
 
     pif = tmr32_if(dut)
-    BUS_TYPE = cocotb.plusargs['BUS_TYPE']
+    BUS_TYPE = cocotb.plusargs["BUS_TYPE"]
     if BUS_TYPE == "APB":
         w_if = bus_apb_if(dut)
     elif BUS_TYPE == "AHB":
@@ -84,93 +92,36 @@ async def module_top(dut):
     # profiler.dump_stats("profile_result.prof")
 
 
-
-
-class base_test(UVMTest):
-    def __init__(self, name="base_test", parent=None):
-        super().__init__(name, parent)
-        self.test_pass = True
-        self.top_env = None
-        self.printer = None
+class tmr_base_test(base_test):
+    def __init__(self, name="flash_first_test", parent=None):
+        BUS_TYPE = cocotb.plusargs["BUS_TYPE"]
+        super().__init__(name, bus_type=BUS_TYPE, parent=parent)
+        self.tag = name
 
     def build_phase(self, phase):
-        # UVMConfigDb.set(self, "example_tb0.bus_env.bus_agent.bus_sequencer.run_phase", "default_sequence", write_seq.type_id.get())
         super().build_phase(phase)
-        # override 
+        # override
         self.set_type_override_by_type(ip_driver.get_type(), tmr32_driver.get_type())
         self.set_type_override_by_type(ip_monitor.get_type(), tmr32_monitor.get_type())
         self.set_type_override_by_type(ref_model.get_type(), tmr32_VIP.get_type())
-        self.set_type_override_by_type(ip_coverage.get_type(), tmr32_coverage.get_type())
+        self.set_type_override_by_type(
+            ip_coverage.get_type(), tmr32_coverage.get_type()
+        )
         self.set_type_override_by_type(ip_logger.get_type(), tmr32_logger.get_type())
-        self.set_type_override_by_type(bus_coverage.get_type(), tmr32_bus_coverage.get_type())
-        BUS_TYPE = cocotb.plusargs['BUS_TYPE']
-        if BUS_TYPE == "AHB":
-            self.set_type_override_by_type(bus_apb_driver.get_type(), bus_ahb_driver.get_type())
-            self.set_type_override_by_type(bus_apb_monitor.get_type(), bus_ahb_monitor.get_type())
-        elif BUS_TYPE == "WISHBONE":
-            self.set_type_override_by_type(bus_apb_driver.get_type(), bus_wb_driver.get_type())
-            self.set_type_override_by_type(bus_apb_monitor.get_type(), bus_wb_monitor.get_type())
-        # self.set_type_override_by_type(scoreboard.get_type(), tmr32_scoreboard.get_type())
-        # Enable transaction recording for everything
-        UVMConfigDb.set(self, "*", "recording_detail", UVM_FULL)
-        # Create the tb
-        self.example_tb0 = top_env.type_id.create("example_tb0", self)
-        # Create a specific depth printer for printing the created topology
-        self.printer = UVMTablePrinter()
-        self.printer.knobs.depth = -1
-
-        arr = []
-        if UVMConfigDb.get(None, "*", "ip_if", arr) is True:
-            UVMConfigDb.set(self, "*", "ip_if", arr[0])
-        else:
-            uvm_fatal("NOVIF", "Could not get ip_if from config DB")
-
-        if UVMConfigDb.get(None, "*", "bus_if", arr) is True:
-            UVMConfigDb.set(self, "*", "bus_if", arr[0])
-        else:
-            uvm_fatal("NOVIF", "Could not get bus_bus_if from config DB")
-        # set max number of uvm errors
-        server = UVMReportServer()
-        server.set_max_quit_count(5)
-        UVMCoreService.get().set_report_server(server)
+        self.set_type_override_by_type(
+            bus_coverage.get_type(), tmr32_bus_coverage.get_type()
+        )
 
 
-    def end_of_elaboration_phase(self, phase):
-        # Set verbosity for the bus monitor for this demo
-        uvm_info(self.get_type_name(), sv.sformatf("Printing the test topology :\n%s", self.sprint(self.printer)), UVM_LOW)
-
-    def start_of_simulation_phase(self, phase):
-        self.bus_sqr = self.example_tb0.bus_env.bus_agent.bus_sequencer
-        self.ip_sqr = self.example_tb0.ip_env.ip_agent.ip_sequencer
-
-    async def run_phase(self, phase):
-        uvm_info("sequence", "Starting test", UVM_LOW)
-
-    def extract_phase(self, phase):
-        super().check_phase(phase)
-        server = UVMCoreService.get().get_report_server()
-        errors = server.get_severity_count(UVM_ERROR)
-        if errors > 0:
-            uvm_fatal("FOUND ERRORS", "There were " + str(errors) + " UVM_ERRORs in the test")
-
-    def report_phase(self, phase):
-        uvm_info(self.get_type_name(), "report_phase", UVM_LOW)
-        if self.test_pass:
-            uvm_info(self.get_type_name(), "** UVM TEST PASSED **", UVM_LOW)
-        else:
-            uvm_fatal(self.get_type_name(), "** UVM TEST FAIL **\n" +
-                self.err_msg)
+uvm_component_utils(tmr_base_test)
 
 
-uvm_component_utils(base_test)
-
-
-class pwm_actions_test(base_test):
+class pwm_actions_test(tmr_base_test):
     def __init__(self, name="pwm_test", parent=None):
         super().__init__(name, parent)
         self.tag = name
 
-    async def run_phase(self, phase):
+    async def main_phase(self, phase):
         uvm_info(self.tag, f"Starting test {self.__class__.__name__}", UVM_LOW)
         phase.raise_objection(self, f"{self.__class__.__name__} OBJECTED")
         bus_seq = pwm_actions_seq("pwm_actions_seq")
@@ -181,12 +132,12 @@ class pwm_actions_test(base_test):
 uvm_component_utils(pwm_actions_test)
 
 
-class pwm_pr_test(base_test):
+class pwm_pr_test(tmr_base_test):
     def __init__(self, name="pwm_test", parent=None):
         super().__init__(name, parent)
         self.tag = name
 
-    async def run_phase(self, phase):
+    async def main_phase(self, phase):
         uvm_info(self.tag, f"Starting test {self.__class__.__name__}", UVM_LOW)
         phase.raise_objection(self, f"{self.__class__.__name__} OBJECTED")
         bus_seq = pwm_pr_seq("pwm_pr_seq")
@@ -197,12 +148,12 @@ class pwm_pr_test(base_test):
 uvm_component_utils(pwm_pr_test)
 
 
-class pwm_tmr_test(base_test):
+class pwm_tmr_test(tmr_base_test):
     def __init__(self, name="pwm_tmr_test", parent=None):
         super().__init__(name, parent)
         self.tag = name
 
-    async def run_phase(self, phase):
+    async def main_phase(self, phase):
         uvm_info(self.tag, f"Starting test {self.__class__.__name__}", UVM_LOW)
         phase.raise_objection(self, f"{self.__class__.__name__} OBJECTED")
         bus_seq = pwm_tmr_seq("pwm_tmr_seq")
@@ -213,12 +164,12 @@ class pwm_tmr_test(base_test):
 uvm_component_utils(pwm_tmr_test)
 
 
-class time_vary_test(base_test):
+class time_vary_test(tmr_base_test):
     def __init__(self, name="tmr32_Try", parent=None):
         super().__init__(name, parent)
         self.tag = name
 
-    async def run_phase(self, phase):
+    async def main_phase(self, phase):
         uvm_info(self.tag, f"Starting test {self.__class__.__name__}", UVM_LOW)
         phase.raise_objection(self, f"{self.__class__.__name__} OBJECTED")
         bus_seq = timer_vary("timer_vary")
@@ -228,12 +179,13 @@ class time_vary_test(base_test):
 
 uvm_component_utils(time_vary_test)
 
-class temp_test(base_test):
+
+class temp_test(tmr_base_test):
     def __init__(self, name="tmr32_Try", parent=None):
         super().__init__(name, parent)
         self.tag = name
 
-    async def run_phase(self, phase):
+    async def main_phase(self, phase):
         uvm_info(self.tag, f"Starting test {self.__class__.__name__}", UVM_LOW)
         phase.raise_objection(self, f"{self.__class__.__name__} OBJECTED")
         bus_seq = temp("timer_vary")
